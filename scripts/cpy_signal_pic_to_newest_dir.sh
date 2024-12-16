10fdcb52a16624bf3f0c30a07c23bea7334bba6f
Mateusz Kusiak (Timax) <timax2@wp.pl>
2024-12-16 21:06:27 +0100

    Scripts: Add script for quick copying signal pics
    Add a script to quickly copy last downloaded picture from signal to the
    newest assets directory. I'll be relying on signal for transfering pictures
    in the future as it has good compression to quality ratio and it's easy
    to transfer photos.

---
+++ scripts/cpy_signal_pic_to_newest_dir.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Check if the current directory path contains "scripts"
+if [[ "$PWD" == *"/scripts"* ]]; then
+    echo "This script must be executed at the root of the repository."
+    exit 1
+fi
+
+# Get the newest file starting with "signal" in the Downloads directory
+newest_signal_file=$(ls -t /Users/"$USER"/Downloads/signal* 2>/dev/null | head -n 1)
+
+# Check if any "signal" file exists
+if [[ -z "$newest_signal_file" ]]; then
+    echo "No Signal pics found in /Users/$USER/Downloads."
+    exit 1
+fi
+
+# Get the newest directory in ./assets/posts/
+newest_post_dir=$(find ./assets/posts -type d -print0 | xargs -0 ls -td | head -n 1)
+
+# Prompt the user for a new filename
+read -p "Enter a new filename (leave blank to keep original): " new_filename
+
+# Extract the file extension
+file_extension="${newest_signal_file##*.}"
+
+# If the user didn't provide a new filename, use the original filename
+if [[ -z "$new_filename" ]]; then
+    new_filename=$(basename "$newest_signal_file")
+else
+    # Add the file extension to the new filename
+    new_filename="${new_filename}.${file_extension}"
+fi
+
+# Copy the file to the newest directory in ./assets/posts/
+cp "$newest_signal_file" "$newest_post_dir/$new_filename"
+
+echo "File copied to $newest_post_dir/$new_filename successfully."
+echo "Add file to post with -> ![${new_filename%%.*}](../.$newest_post_dir/$new_filename)"
