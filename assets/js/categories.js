921dafdacfd66f08a58667f899c4ccfbce73ec0b
Mateusz Kusiak (Timax) <timax2@wp.pl>
2025-02-15 19:11:04 +0100

    Categories: Fix categories starting with numbers
    The script categories.js failed when there were categories starting with
    numbers. The solution was to wrap categories with quotations. The code
    for this is still cursed, I splited it into lines but I ain't going to
    touch it further.

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,9 +1,20 @@
 ---
 ---
 
-const categories = { {% for category in site.categories %}{% capture category_name %}{{ category | first }}{% endcapture %}{{ category_name | replace: " ", "_" }}: [{% for post in site.categories[category_name] %}{ url: `{{ site.baseurl }}{{ post.url }}`, date: `{{post.date | date_to_string}}`, title: `{{post.title}}`},{% endfor %}],{% endfor %} }
+// OMFG, this is war crime right here
+const categories = {
+  {% for category in site.categories %}
+    {% capture category_name %}{{ category | first }}{% endcapture %}
+    "{{ category_name | replace: " ", "_" }}": [
+      {% for post in site.categories[category_name] %}{
+        url: `{{ site.baseurl }}{{ post.url }}`,
+        date: `{{ post.date | date_to_string }}`,
+        title: `{{ post.title }}`},
+      {% endfor %}],
+    {% endfor %}
+};
 
-console.log(categories)
+//console.log(categories)
 
 window.onload = function () {
   document.querySelectorAll(".category").forEach((category) => {
